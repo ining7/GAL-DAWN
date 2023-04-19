a7a2785fd519f34e77b1ff6ce12ec930082bab5a
lxrzlyr <73328734+lxrzlyr@users.noreply.github.com>
2023-04-19 15:35:23 +0800

    Update and new feature
    convert.cu generates a conversion program to convert COO's mtx files into RCC and CRC matrices.
    Update v4, reduce memory usage.
    dawn_cpu_big.cu: Added large graph processing function, which can process graphs with 214M nodes and 232M edges on 20G memory.

--- dawn_cpu_v4.cu
+++ dawn_cpu_v4.cu
@@ -7,7 +7,7 @@ struct Matrix
 {
     int rows;
     int cols;
-    int nnz;
+    uint64_t nnz;
     int **A;      // 按列压缩
     int *A_entry; // 每列项数
     int **B;      // 按行压缩
@@ -17,10 +17,90 @@ struct Matrix
 };
 
 void runDawn(Matrix &matrix, string &input_path, string &output_path);
-void readgraph(string &input_path, Matrix &matrix);
+void createGraph(string &input_path, Matrix &matrix);
+void readGraph(string &input_path, Matrix &matrix, vector<pair<int, int>> &cooMatCol);
+void COO2CRC(Matrix &matrix, vector<pair<int, int>> &cooMatCol);
+void COO2RCC(Matrix &matrix, vector<pair<int, int>> &cooMatRow);
 float dawnSssp(Matrix &matrix, int source);
 
-void readgraph(string &input_path, Matrix &matrix)
+void COO2CRC(Matrix &matrix, vector<pair<int, int>> &cooMatCol)
+{
+    int col_a = 0;
+    int k = 0;
+    vector<int> tmp;
+    tmp.clear();
+    while (k < cooMatCol.size())
+    {
+        if (matrix.A_entry[col_a] != 0)
+        {
+            if (cooMatCol[k].second == col_a)
+            {
+                tmp.push_back(cooMatCol[k].first);
+                k++;
+            }
+            else
+            {
+#pragma omp parallel for
+                for (int j = 0; j < matrix.A_entry[col_a]; j++)
+                {
+                    matrix.A[col_a][j] = tmp[j];
+                }
+                tmp.clear();
+                col_a++;
+            }
+        }
+        else
+        {
+            col_a++;
+        }
+    }
+#pragma omp parallel for
+    for (int j = 0; j < matrix.A_entry[col_a]; j++)
+    {
+        matrix.A[col_a][j] = tmp[j];
+    }
+}
+
+void COO2RCC(Matrix &matrix, vector<pair<int, int>> &cooMatRow)
+{
+    int row_b = 0;
+    int k = 0;
+    vector<int> tmp;
+    tmp.clear();
+    while (k < cooMatRow.size())
+    {
+        if (matrix.A_entry[row_b] != 0)
+        {
+            if (cooMatRow[k].first == row_b)
+            {
+                tmp.push_back(cooMatRow[k].second);
+                k++;
+            }
+            else
+            {
+#pragma omp parallel for
+                for (int j = 0; j < matrix.B_entry[row_b]; j++)
+                {
+                    matrix.B[row_b][j] = tmp[j];
+                }
+                tmp.clear();
+                row_b++;
+            }
+        }
+        else
+        {
+            row_b++;
+        }
+    }
+#pragma omp parallel for
+    for (int j = 0; j < matrix.B_entry[row_b]; j++)
+    {
+        matrix.B[row_b][j] = 0;
+        matrix.B[row_b][j] = tmp[j];
+    }
+}
+
+void readCRC(Matrix &matrix, string &input_path)
 {
     std::ifstream file(input_path);
     if (!file.is_open())
@@ -28,92 +108,134 @@ void readgraph(string &input_path, Matrix &matrix)
         std::cerr << "Error opening file " << input_path << std::endl;
         return;
     }
+    matrix.A = new int *[matrix.rows];
+    string line;
+    int rows = 0, cols = 0, k = 0;
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        ss >> cols;
+        if (cols == 0)
+        {
+            matrix.A_entry[rows] = 0;
+            rows++;
+            k = 0;
+            continue;
+        }
+        matrix.A_entry[rows] = cols;
+        matrix.A[rows] = new int[cols];
+        for (int j = 0; j < cols; j++)
+        {
+            ss >> matrix.A[rows][k++];
+        }
+        rows++;
+        k = 0;
+    }
+}
+void readRCC(Matrix &matrix, string &input_path)
+{
+    std::ifstream file(input_path);
+    if (!file.is_open())
+    {
+        std::cerr << "Error opening file " << input_path << std::endl;
+        return;
+    }
+    matrix.B = new int *[matrix.rows];
+    string line;
+    int rows = 0, cols = 0, k = 0;
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        ss >> cols;
+        if (cols == 0)
+        {
+            matrix.B_entry[rows] = 0;
+            rows++;
+            k = 0;
+            continue;
+        }
+        matrix.B_entry[rows] = cols;
+        matrix.B[rows] = new int[cols];
+        for (int j = 0; j < cols; j++)
+        {
+            ss >> matrix.B[rows][k++];
+        }
+        rows++;
+        k = 0;
+    }
+}
 
+void readGraph(string &input_path, Matrix &matrix, vector<pair<int, int>> &cooMatCol)
+{
+    std::ifstream file(input_path);
+    if (!file.is_open())
+    {
+        std::cerr << "Error opening file " << input_path << std::endl;
+        return;
+    }
     std::string line;
-    int rows, cols, nnz, dim;
+
+    int rows, cols;
     while (std::getline(file, line))
     {
         if (line[0] == '%')
             continue;
-
-        std::stringstream ss(line);
-        ss >> rows >> cols >> nnz >> dim;
-        break;
-    }
-    cout << rows << " " << cols << " " << nnz << " " << dim << endl;
-    matrix.rows = rows;
-    matrix.cols = cols;
-    matrix.nnz = nnz;
-    matrix.dim = dim;
-
-    matrix.A = new int *[matrix.rows];
-    matrix.A_entry = new int[matrix.rows];
-    matrix.B = new int *[matrix.rows];
-    matrix.B_entry = new int[matrix.rows];
-    bool **tmp = new bool *[matrix.rows];
-#pragma omp parallel for
-    for (int i = 0; i < matrix.rows; i++)
-    {
-        tmp[i] = new bool[matrix.rows];
-        matrix.A_entry[i] = 0;
-        matrix.B_entry[i] = 0;
-    }
-    while (std::getline(file, line))
-    {
         std::stringstream ss(line);
         ss >> rows >> cols;
         rows--;
         cols--;
         if (rows != cols)
         {
-            tmp[rows][cols] = true;
+            cooMatCol.push_back({rows, cols});
+            matrix.A_entry[cols]++;
+            matrix.B_entry[rows]++;
+            matrix.entry++;
         }
     }
     file.close();
 
+    matrix.nnz = matrix.entry;
+    cout << "nnz: " << matrix.nnz << endl;
+}
+
+void createGraph(string &input_path, Matrix &matrix)
+{
+
+    matrix.A = new int *[matrix.rows];
+    matrix.A_entry = new int[matrix.rows];
+    matrix.B = new int *[matrix.rows];
+    matrix.B_entry = new int[matrix.rows];
+    matrix.entry = 0;
 #pragma omp parallel for
     for (int i = 0; i < matrix.rows; i++)
     {
-        vector<int> tmp_A;
-        vector<int> tmp_B;
-        tmp_A.clear();
-        tmp_B.clear();
-        for (int j = 0; j < matrix.cols; j++)
-        {
-            if (tmp[j][i] == true && i != j)
-            {
-                tmp_A.push_back(j);
-                matrix.A_entry[i]++;
-            }
-            if (tmp[i][j] == true && i != j)
-            {
-                tmp_B.push_back(j);
-                matrix.B_entry[i]++;
-            }
-        }
+        matrix.A_entry[i] = 0;
+        matrix.B_entry[i] = 0;
+    }
+
+    vector<pair<int, int>> cooMatCol;
+    cout << "Read Input Graph" << endl;
+    readGraph(input_path, matrix, cooMatCol);
+
+    cout << "Create Transport Graph" << endl;
+    vector<pair<int, int>> cooMatRow = cooMatCol;
+    sort(cooMatRow.begin(), cooMatRow.end());
+
+    cout << "Create Input Matrices" << endl;
+#pragma omp parallel for
+    for (int i = 0; i < matrix.rows; i++)
+    {
         matrix.A[i] = new int[matrix.A_entry[i]];
         matrix.B[i] = new int[matrix.B_entry[i]];
-        for (int j = 0; j < matrix.A_entry[i]; j++)
-        {
-            matrix.A[i][j] = tmp_A[j];
-        }
-        for (int j = 0; j < matrix.B_entry[i]; j++)
-        {
-            matrix.B[i][j] = tmp_B[j];
-        }
     }
-    cout << "Initialize input matrices" << endl;
-#pragma omp parallel for
-    for (int i = 0; i < matrix.rows; i++)
-    {
-        delete[] tmp[i];
-    }
-    delete[] tmp;
+    COO2CRC(matrix, cooMatCol);
+    COO2RCC(matrix, cooMatRow);
+    cout << "Initialize Input Matrices" << endl;
 }
 
 void runDawn(Matrix &matrix, string &input_path, string &output_path)
 {
-    readgraph(input_path, matrix);
+    createGraph(input_path, matrix);
 
     std::ofstream outfile(output_path);
     if (!outfile.is_open())
@@ -122,10 +244,10 @@ void runDawn(Matrix &matrix, string &input_path, string &output_path)
         return;
     }
     float elapsed_time = 0.0;
-    int entry = 0;
-    int thread = 12;
+    int proEntry = 0;
+    int thread = 20;
     std::cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>> APSP start <<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
-#pragma omp parallel for num_threads(12)
+#pragma omp parallel for
     for (int i = 0; i < matrix.rows; i++)
     {
         float time_tmp = 0.0f;
@@ -135,14 +257,14 @@ void runDawn(Matrix &matrix, string &input_path, string &output_path)
 #pragma omp critical
         {
             elapsed_time += time_tmp;
-            entry++;
+            proEntry++;
         }
         // 输出结果
 
-        if (entry % (matrix.rows / 100) == 0)
+        if (proEntry % (matrix.rows / 100) == 0)
         {
             float completion_percentage =
-                static_cast<float>(entry * 100.0f) / static_cast<float>(matrix.rows);
+                static_cast<float>(proEntry * 100.0f) / static_cast<float>(matrix.rows);
             std::cout << "Progress: " << completion_percentage << "%" << std::endl;
             std::cout << "Elapsed Time :" << elapsed_time / (thread * 1000) << " s" << std::endl;
         }
@@ -243,7 +365,24 @@ int main(int argc, char *argv[])
         return 0;
     }
 
+    std::string line;
+    int rows, cols, nnz, dim;
+    while (std::getline(file, line))
+    {
+        if (line[0] == '%')
+            continue;
+
+        std::stringstream ss(line);
+        ss >> rows >> cols >> nnz >> dim;
+        break;
+    }
+    cout << rows << " " << cols << " " << nnz << " " << dim << endl;
+
     Matrix matrix;
+    matrix.rows = rows;
+    matrix.cols = cols;
+    matrix.dim = dim;
+
     runDawn(matrix, input_path, output_path);
     return 0;
 }
