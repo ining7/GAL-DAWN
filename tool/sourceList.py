d997a5222f1a5f7a20d81b88ab7ea4fc96f0724d
lxr <1289539524@qq.com>
2023-05-21 21:30:09 +0800

    Update:Update tools and decumentation 	修改：     MPI/CMakeLists.txt 	修改：     README.md 	修改：     include/dawn.cuh 	修改：     readmeforCPU.md 	删除：     tool/Draw_figure.py 	修改：     tool/check_by_networkx.py 	删除：     tool/convert.cu 	新文件：   tool/latency.py 	重命名：   tool/process_v2v.sh -> tool/process_all.sh 	新文件：   tool/scalability.py 	修改：     tool/sourceList.py 	新文件：   tool/speedup.py
    	修改：     gunrock/apsp.cu

--- tool/sourceList.py
+++ tool/sourceList.py
@@ -8,6 +8,5 @@ num_of_randoms = int(sys.argv[1])
 with open("sourceList.txt", "w") as file:
     # 生成指定数量的随机整数并将它们写入文件中
     for i in range(num_of_randoms):
-        rand_int = random.randint(0, 2**31)
+        rand_int = random.randint(0, pow(2, 31))
         file.write(str(rand_int) + "\n")
-
